996a02dfee43a4b4c0f78518aa513446ee20720d
Bianca Pinheiro <124315313+ironacon@users.noreply.github.com>
2024-06-23 16:10:32 -0300

    Delete PROVA directory

--- PROVA/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dbHost = 'localhost';
-    $dbUsername = 'root';
-    $dbPassword = '';
-    $dbName = 'create-account';
-
-    $conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
-
--- PROVA/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['email']);
-unset($_SESSION['password']);
-header("Location: login.php");
